fe574b3de9fd3946c9a2cd0d2167c5dae678a8e3
Erick-A-Martins <erickalvesqmartins@gmail.com>
2025-12-15 09:38:08 -0300

    more examples of aggregation

--- Modulo_3/agregacoes.sql
+++ Modulo_3/agregacoes.sql
@@ -98,3 +98,16 @@ SELECT oi.product_id,
   FROM order_items oi
   GROUP BY oi.product_id
   HAVING SUM(oi.quantity) > 3;
+
+-- Mais exemplos de agregações e limit
+SELECT oi.product_id, SUM(oi.quantity) AS qtd_total
+  FROM order_items oi
+  GROUP BY oi.product_id
+  ORDER BY SUM(oi.quantity) DESC
+  LIMIT 10;
+
+SELECT o.customer_id, SUM(o.total_amount) AS faturamento_total
+  FROM orders o
+  GROUP BY o.customer_id
+  ORDER BY faturamento_total ASC -- Utilizando alias no ORDER BY
+  LIMIT 5;
