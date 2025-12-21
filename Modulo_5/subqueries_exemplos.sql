ba078fcd8581a71fd3d28d5aeac4a52b33f640d7
Erick-A-Martins <erickalvesqmartins@gmail.com>
2025-12-21 10:51:09 -0300

    more examples of subqueries

---
+++ Modulo_5/subqueries_exemplos.sql
@@ -0,0 +1,61 @@
+-- Uso de subqueries (não correlacionadas)
+
+SELECT customer_id, first_name, last_name
+FROM customers
+WHERE customer_id IN (
+	SELECT customer_id
+	FROM orders
+);
+
+SELECT product_name, price
+FROM products
+WHERE price > (
+	SELECT AVG(price) + 100
+	FROM products
+);
+
+SELECT order_id, total_amount
+FROM orders
+WHERE total_amount = (
+	SELECT max(total_amount)
+	FROM orders
+);
+
+SELECT product_id, product_name
+FROM products p
+WHERE EXISTS (
+	SELECT 1
+	FROM order_items oi
+	WHERE oi.product_id = p.product_id
+);
+
+-- Exemplo mais complexo
+SELECT
+	e.employee_id,
+	e.first_name || ' ' || e.last_name AS nome,
+	d.department_name,
+	s.salary_amount
+FROM employees AS e
+INNER JOIN departments AS d
+	ON e.department_id = d.department_id
+INNER JOIN salaries AS s
+	ON e.employee_id = s.employee_id
+	AND s.effective_to IS NULL
+WHERE s.salary_amount  > (
+	SELECT AVG(s2.salary_amount)
+	FROM employees AS e2
+	INNER JOIN salaries AS s2
+		ON e2.employee_id = s2.employee_id
+		AND s2.effective_to IS NULL
+	WHERE e2.department_id = e.department_id 
+)
+ORDER BY d.department_name, s.salary_amount DESC;
+
+-- Subquery correlacionada
+SELECT 
+	p1.product_name,
+	p1.price,
+	(SELECT AVG(price)
+	FROM products p2
+	WHERE p2.category_id = p1.category_id) AS media_categoria
+FROM products p1;
